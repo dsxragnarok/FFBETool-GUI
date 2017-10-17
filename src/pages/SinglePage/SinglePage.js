5a13f67e642f34cfb76fcc95c2c1b2ea8f1c41d6
Kevin Phung <kevin.phung@nagrastar.com>
2017-10-16 21:07:36 -0600

    feat: create 3 dropzones
    The dropzones will act to accept the requisite source files

--- src/pages/SinglePage/SinglePage.js
+++ src/pages/SinglePage/SinglePage.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class SinglePage extends Component {
   static displayName = 'SinglePage'
@@ -6,6 +8,20 @@ export default class SinglePage extends Component {
   static propTypes = {}
 
   render () {
-    return <div><h2>I am the Single Page</h2></div>;
+    return (
+      <div>
+        <Dropzone name="select-anime" accept="image/png" multiple={ false }>
+          <RaisedButton label="SELECT ANIME" />
+        </Dropzone>
+
+        <Dropzone name="select-cgg" accept="text/csv" multiple={ false }>
+          <RaisedButton label="SELECT CGG" />
+        </Dropzone>
+
+        <Dropzone name="select-cgs" accept="text/csv">
+          <RaisedButton label="SELECT CGS" />
+        </Dropzone>
+      </div>
+    );
   }
 }
--- src/pages/SinglePage/__tests__/SinglePage.test.js
+++ src/pages/SinglePage/__tests__/SinglePage.test.js
@@ -1,9 +1,109 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import Dropzone from 'react-dropzone';
+import RaisedButton from 'material-ui/RaisedButton';
+
 import SinglePage from '../SinglePage';
+import { renderComponent } from '../../../testUtils/reactHelpers';
+
+const renderPage = (props) => renderComponent(SinglePage, props);
 
 describe('SinglePage component', () => {
   it('renders', () => {
-    expect(shallow(<SinglePage />)).toBePresent();
+    expect(renderPage()).toBePresent();
+  });
+
+  it('renders three Dropzone components', () => {
+    const component = renderPage();
+    const dropzones = component.find(Dropzone);
+
+    expect(dropzones.length).toBe(3);
+  });
+
+  describe('Anime Dropzone', () => {
+    let dropzone = null;
+
+    beforeEach(() => {
+      dropzone = renderPage()
+        .find(Dropzone)
+        .findWhere((elem) => elem.prop('name') === 'select-anime');
+    });
+
+    afterEach(() => {
+      dropzone = null;
+    });
+
+    it('renders a Dropzone component that only accepts "image/png"', () => {
+      expect(dropzone.prop('accept')).toBe('image/png');
+    });
+
+    it('renders a Dropzone component that only accepts a single "image/png"', () => {
+      expect(dropzone.prop('multiple')).toBe(false);
+    });
+
+    it('renders a button labeled "SELECT ANIME"', () => {
+      const button = dropzone.find(RaisedButton)
+        .findWhere((btn) => btn.prop('label') === 'SELECT ANIME');
+
+      expect(button).toBePresent();
+    });
+  });
+
+  describe('Cgg Dropzone', () => {
+    let dropzone = null;
+
+    beforeEach(() => {
+      dropzone = renderPage().find(Dropzone)
+        .findWhere((elem) => elem.prop('name') === 'select-cgg');
+    });
+    afterEach(() => dropzone = null);
+
+    it('renders a Dropzone component for "cgg" file', () => {
+      expect(dropzone).toBePresent();
+    });
+
+    it('renders a Dropzone component that only accepts "text/csv"', () => {
+      expect(dropzone.prop('accept')).toBe('text/csv');
+    });
+
+    it('renders a Dropzone component that only accepts a single "text/csv"', () => {
+      expect(dropzone.prop('multiple')).toBe(false);
+    });
+
+    it('renders a button labeled "SELECT CGG"', () => {
+      const button = dropzone
+        .find(RaisedButton)
+        .findWhere((btn) => btn.prop('label') === 'SELECT CGG');
+
+      expect(button).toBePresent();
+    });
+  });
+
+  describe('Cgs Dropzone', () => {
+    let dropzone = null;
+
+    beforeEach(() => {
+      dropzone = renderPage().find(Dropzone)
+        .findWhere((elem) => elem.prop('name') === 'select-cgs');
+    });
+    afterEach(() => dropzone = null);
+
+    it('renders a Dropzone for "cgs" files', () => {
+      expect(dropzone).toBePresent();
+    });
+
+    it('renders a Dropzone component that only accepts "text/csv"', () => {
+      expect(dropzone.prop('accept')).toBe('text/csv');
+    });
+
+    it('renders a Dropzone component that accepts multiple "text/csv"', () => {
+      expect(dropzone.prop('multiple')).toBe(true);
+    });
+
+    it('renders a button labeled "SELECT CGS"', () => {
+      const button = dropzone
+        .find(RaisedButton)
+        .findWhere((btn) => btn.prop('label') === 'SELECT CGS');
+
+      expect(button).toBePresent();
+    });
   });
 });
