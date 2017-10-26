e8f74e10df7a062d51330fb16da44f5e4fe239f0
Kevin Phung <kevin.phung@nagrastar.com>
2017-10-26 14:11:22 -0500

    feat: implement state actions for SingleProcessPage

--- src/pages/SingleProcessPage/__tests__/actions.test.js
+++ src/pages/SingleProcessPage/__tests__/actions.test.js
@@ -2,17 +2,7 @@ import pick from 'lodash/pick';
 
 import * as actions from '../actions';
 import * as types from '../action-types';
-
-const mockFile = {
-  lastModified : 1507666290000,
-  lastModifiedDate : new Date(1507666290000),
-  name: 'unit_anime_253001205',
-  path: '/Users/erdrick.loto/Share/ffbe-2.3.1/unit_anime_253001205.png',
-  preview: 'blob:http://localhost:3000/616351f0-27bc-42d3-b6fd-fdd9f4f28d06',
-  size: 56324,
-  type: 'image/png',
-  webkitRelativePath: ''
-};
+import { mockCggFile, mockCgsFiles, mockPngFile } from '../__mocks__/fileMocks';
 
 describe('SingleProcess actions', () => {
   describe('loadAnimeFile', () => {
@@ -52,9 +42,9 @@ describe('SingleProcess actions', () => {
     it('creates the appropriate action and payload', () => {
       const expectedAction = {
         type: ADD_ANIME_FILE,
-        payload: pick(mockFile, ['lastModified', 'name', 'path', 'preview'])
+        payload: pick(mockPngFile, ['lastModified', 'name', 'path', 'preview'])
       };
-      const action = addAnimeFile(mockFile);
+      const action = addAnimeFile(mockPngFile);
 
       expect(action).toEqual(expectedAction);
     });
@@ -73,30 +63,155 @@ describe('SingleProcess actions', () => {
       expect(action).toEqual(expectedAction);
     });
   });
+
+  describe('loadCggFile', () => {
+    const { LOAD_CGG_FILE } = types;
+    const { loadCggFile } = actions;
+
+    it('create the appropriate action and payload', () => {
+      const expectedAction = {
+        type: LOAD_CGG_FILE,
+        payload: true
+      };
+      const action = loadCggFile(true);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+
+  describe('cggFileErrored', () => {
+    const { CGG_FILE_ERROR } = types;
+    const { cggFileError } = actions;
+
+    it('creates the appropriate action and payload', () => {
+      const expectedAction = {
+        type: CGG_FILE_ERROR,
+        payload: 'The error of file cgg'
+      };
+      const action = cggFileError('The error of file cgg');
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+
+  describe('addCggFile', () => {
+    const { ADD_CGG_FILE } = types;
+    const { addCggFile } = actions;
+
+    it('creates the appropriate action and payload', () => {
+      const expectedAction = {
+        type: ADD_CGG_FILE,
+        payload: pick(mockCggFile, ['name', 'path', 'lastModified'])
+      };
+      const action = addCggFile(mockCggFile);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+
+  describe('removeCggFile', () => {
+    const { REMOVE_CGG_FILE } = types;
+    const { removeCggFile } = actions;
+
+    it('creates the appropriate action and payload', () => {
+      const expectedAction = {
+        type: REMOVE_CGG_FILE
+      };
+      const action = removeCggFile();
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+
+  describe('loadCgsFile', () => {
+    const { LOAD_CGS_FILE } = types;
+    const { loadCgsFile } = actions;
+
+    it('creates the appropriate action and payload', () => {
+      const expectedAction = {
+        type: LOAD_CGS_FILE,
+        payload: true
+      };
+      const action = loadCgsFile(true);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+
+  describe('cgsFileErrored', () => {
+    const { CGS_FILE_ERROR } = types;
+    const { cgsFileError } = actions;
+
+    it('creates the appropriate action and payload', () => {
+      const expectedAction = {
+        type: CGS_FILE_ERROR,
+        payload: 'You are the error!'
+      };
+      const action = cgsFileError('You are the error!');
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+
+  describe('addCgsFile', () => {
+    const { ADD_CGS_FILE } = types;
+    const { addCgsFile } = actions;
+
+    it('creates the appropriate action and payload', () => {
+      const mockCgsFile = mockCgsFiles[0];
+      const expectedAction = {
+        type: ADD_CGS_FILE,
+        payload: pick(mockCgsFile, ['name', 'path', 'lastModified'])
+      };
+      const action = addCgsFile(mockCgsFile);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+
+  describe('removeCgsFile', () => {
+    const { REMOVE_CGS_FILE } = types;
+    const { removeCgsFile } = actions;
+
+    it('creates the appropriate action and payload', () => {
+      const fileToRemove = mockCgsFiles[1];
+      const { name, path } = fileToRemove;
+      const expectedAction = {
+        type: REMOVE_CGS_FILE,
+        payload: { name, path }
+      };
+      const action = removeCgsFile({ name, path });
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+
+  describe('batchAddCgsFiles', () => {
+    const { BATCH_ADD_CGS_FILES } = types;
+    const { batchAddCgsFiles } = actions;
+
+    it('creates the appropriate action and payload', () => {
+      const expectedAction = {
+        type: BATCH_ADD_CGS_FILES,
+        payload: mockCgsFiles.map(({ name, path, lastModified }) => ({ name, path, lastModified }))
+      };
+      const action = batchAddCgsFiles(mockCgsFiles);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+
+  describe('clearCgsFiles', () => {
+    const { CLEAR_CGS_FILES } = types;
+    const { clearCgsFiles } = actions;
+
+    it('creates the appropriate action and payload', () => {
+      const expectedAction = {
+        type: CLEAR_CGS_FILES
+      };
+      const action = clearCgsFiles();
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
 });
-
-/*
-
-I want to add an anime file
-  * Anime File Loading  -> LOAD_ANIME_FILE
-  * Anime File Error  -> ANIME_FILE_ERROR
-  * Add Anime File  -> ADD_ANIME_FILE
-  * Remove Anime File  -> REMOVE_ANIME_FILE
-
-I want to add a cgg file
-  * Cgg File Loading -> LOAD_CGG_FILE
-  * Cgg File Error  -> CGG_FILE_ERROR
-  * Add Cgg File  -> ADD_CGG_FILE
-  * Remove Cgg File -> REMOVE_CGG_FILE
-
-I want to add a cgs file
-  * Cgs File Loading -> LOAD_CGS_FILE
-  * Cgs File Error -> CGS_FILE_ERROR
-  * Add Cgs File -> ADD_CGS_FILE
-  * Remove Cgs File -> REMOVE_CGS_FILE
-
-I want to add multiple cgs files
-  * Batch add Cgs File -> BATCH_ADD_CGS_FILES
-  * Clear Cgs Files -> CLEAR_CGS_FILES
-
-*/
--- src/pages/SingleProcessPage/actions.js
+++ src/pages/SingleProcessPage/actions.js
@@ -4,7 +4,17 @@ import {
   LOAD_ANIME_FILE,
   ANIME_FILE_ERROR,
   ADD_ANIME_FILE,
-  REMOVE_ANIME_FILE
+  REMOVE_ANIME_FILE,
+  LOAD_CGG_FILE,
+  CGG_FILE_ERROR,
+  ADD_CGG_FILE,
+  REMOVE_CGG_FILE,
+  LOAD_CGS_FILE,
+  CGS_FILE_ERROR,
+  ADD_CGS_FILE,
+  REMOVE_CGS_FILE,
+  BATCH_ADD_CGS_FILES,
+  CLEAR_CGS_FILES
 } from './action-types';
 
 export const loadAnimeFile = createAction(LOAD_ANIME_FILE);
@@ -14,6 +24,28 @@ export const addAnimeFile = createAction(
   (file) => pick(file, ['lastModified', 'name', 'path', 'preview'])
 );
 export const removeAnimeFile = createAction(REMOVE_ANIME_FILE);
+
+export const loadCggFile = createAction(LOAD_CGG_FILE);
+export const cggFileError = createAction(CGG_FILE_ERROR);
+export const addCggFile = createAction(
+  ADD_CGG_FILE,
+  (file) => pick(file, ['name', 'path', 'lastModified'])
+);
+export const removeCggFile = createAction(REMOVE_CGG_FILE);
+
+export const loadCgsFile = createAction(LOAD_CGS_FILE);
+export const cgsFileError = createAction(CGS_FILE_ERROR);
+export const addCgsFile = createAction(
+  ADD_CGS_FILE,
+  (file) => pick(file, ['lastModified', 'name', 'path'])
+);
+export const removeCgsFile = createAction(REMOVE_CGS_FILE);
+
+export const batchAddCgsFiles = createAction(
+  BATCH_ADD_CGS_FILES,
+  (files) => files.map((file) => pick(file, ['lastModified', 'name', 'path']))
+);
+export const clearCgsFiles = createAction(CLEAR_CGS_FILES);
 /*
 
 I want to add an anime file
