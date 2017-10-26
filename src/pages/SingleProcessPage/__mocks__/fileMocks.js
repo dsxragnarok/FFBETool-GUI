a67deddeb2c0695dff11864d982591e5e451513b
Kevin Phung <kevin.phung@nagrastar.com>
2017-10-26 14:11:44 -0500

    test: add mock files json for testing

---
+++ src/pages/SingleProcessPage/__mocks__/fileMocks.js
@@ -0,0 +1,48 @@
+export const mockPngFile = {
+  lastModified : 1507666290000,
+  lastModifiedDate : new Date(1507666290000),
+  name: 'unit_anime_253001205',
+  path: '/Users/erdrick.loto/Share/ffbe-2.3.1/unit_anime_253001205.png',
+  preview: 'blob:http://localhost:3000/616351f0-27bc-42d3-b6fd-fdd9f4f28d06',
+  size: 56324,
+  type: 'image/png',
+  webkitRelativePath: ''
+};
+export const mockCggFile = {
+  lastModified : 1508967837495,
+  lastModifiedDate : new Date(1508967837495),
+  name: 'unit_cgg_253001205',
+  path: '/Users/erdrick.loto/Share/ffbe-2.3.1/unit_cgg_253001205.csv',
+  preview: 'blob:http://localhost:3000/616351f0-27bc-42d3-b6fd-fdd9f4f28d06',
+  size: 56324,
+  type: 'text/csv',
+  webkitRelativePath: ''
+};
+export const mockCgsFiles = [{
+  lastModified : 1834727837495,
+  lastModifiedDate : new Date(1834727837495),
+  name: 'unit_atk_cgg_253001205',
+  path: '/Users/erdrick.loto/Share/ffbe-2.3.1/unit_atk_cgg_253001205.csv',
+  preview: 'blob:http://localhost:3000/616351f0-27bc-42d3-b6fd-fdd9f4f28d06',
+  size: 56324,
+  type: 'text/csv',
+  webkitRelativePath: ''
+}, {
+  lastModified : 1508929834495,
+  lastModifiedDate : new Date(1508929834495),
+  name: 'unit_limit_atk_cgg_253001205',
+  path: '/Users/erdrick.loto/Share/ffbe-2.3.1/unit_limit_atk_cgg_253001205.csv',
+  preview: 'blob:http://localhost:3000/616351f0-27bc-42d3-b6fd-fdd9f4f28d06',
+  size: 56324,
+  type: 'text/csv',
+  webkitRelativePath: ''
+}, {
+  lastModified : 1508982347495,
+  lastModifiedDate : new Date(1508982347495),
+  name: 'unit_idle_cgg_253001205',
+  path: '/Users/erdrick.loto/Share/ffbe-2.3.1/unit_idle_cgg_253001205.csv',
+  preview: 'blob:http://localhost:3000/616351f0-27bc-42d3-b6fd-fdd9f4f28d06',
+  size: 56324,
+  type: 'text/csv',
+  webkitRelativePath: ''
+}];
